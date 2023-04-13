05761df80316d5f08d6a0951d85fc757382b0ce5
U071776 <muhammad_sudibyo@bca.co.id>
2023-04-13 15:40:43 +0700

    phase 2

--- src/main/java/id/co/funtourntravel/user/controller/UserController.java
+++ src/main/java/id/co/funtourntravel/user/controller/UserController.java
@@ -1,74 +1,66 @@
 package id.co.funtourntravel.user.controller;
 
 import id.co.funtourntravel.user.model.User;
+import id.co.funtourntravel.user.model.dto.UserDTO;
 import id.co.funtourntravel.user.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@Controller
+@RestController
 @RequestMapping("user")
 public class UserController {
 
     @Autowired
     UserService userService;
 
-    @GetMapping("/id")
-    public @ResponseBody User getUserId(@RequestParam("id") int id){
+    private  final Logger log = LoggerFactory.getLogger(this.getClass());
+
+
+
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody List getAll(){
+        return userService.getAllUser();
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDTO addEmployee(@RequestBody UserDTO model){
+        User user = new User();
+        BeanUtils.copyProperties(model, user);
+        userService.insert(user);
+        return model;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public User getUserById(@PathVariable("id") int id){
         User user = new User();
         user.setId(id);
         return userService.getUserById(user);
     }
 
-    @GetMapping("/all")
-    public @ResponseBody List getAll(){
-        return userService.getAllUser();
-    }
-
-
-
-    @GetMapping("/update")
-    public String updateUser(@RequestParam("userId") String userId,
-                                 @RequestParam("name") String name,
-                                 @RequestParam("email") String email,
-                                 @RequestParam("password") String password){
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDTO updateUser(@RequestBody UserDTO model, @PathVariable("id") int id){
         User user = new User();
-        user.setName(name);
-        user.setUserId(userId);
-        user.setEmail(email);
-        user.setPassword(password);
-        return userService.update(user);
-
-    }
-
-    @GetMapping("/insert")
-    public String insertUser(@RequestParam("userId") String userId,
-                             @RequestParam("name") String name,
-                             @RequestParam("email") String email,
-                             @RequestParam("password") String password){
-        User user = new User();
-        user.setName(name);
-        user.setUserId(userId);
-        user.setEmail(email);
-        user.setPassword(password);
+        log.info("disni id nya = "+id);
+        model.setId(id);
         userService.insert(user);
-        return backToIndex();
+        return  model;
     }
 
-    @GetMapping("/delete")
-    public String deleteEmployee(@RequestParam("id") int id){
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteUser(@PathVariable("id") int id){
         User user = new User();
         user.setId(id);
         userService.delete(user);
-        return backToIndex();
-    }
-
-    public String backToIndex(){
-        //return "your transaction has ben executed";
-        return "redirect:/user/all";
     }
 }
---
+++ src/main/java/id/co/funtourntravel/user/model/dto/UserDTO.java
@@ -0,0 +1,60 @@
+package id.co.funtourntravel.user.model.dto;
+
+import jakarta.persistence.Column;
+
+public class UserDTO {
+
+    private int id;
+
+
+    private String userId;
+
+
+    private String name;
+
+
+    private String email;
+
+
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
